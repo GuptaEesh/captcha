d41f0627c83e82562bda578f0e253552c3255439
GuptaEesh <p1r8a5t7ibha@gmail.com>
2023-03-26 22:55:33 +0530

    [feat] completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const makeCaptcha = () => {
+  const randomChars = "ABCabdewadsad123123234DDFEFs";
+  let captcha = "";
+  for (let i = 0; i < 5; i++) {
+    captcha += randomChars.charAt(
+      Math.floor(Math.random() * randomChars.length)
+    );
+  }
+  return captcha;
+};
 
-function App() {
+const App = () => {
+  const [captcha, setCaptcha] = useState(makeCaptcha());
+  const [value, setValue] = useState("");
+
+  const inputHandler = (e) => {
+    setValue(e.target.value);
+  };
+
+  const regenerateCaptcha = (e) => {
+    e.preventDefault();
+    setCaptcha(makeCaptcha());
+    setValue("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value === captcha) {
+      alert("Sign in successful");
+    } else {
+      alert("Captcha is invalid. Please try again.");
+      setCaptcha(makeCaptcha());
+      setValue("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} className="form">
+      <h1>Simple Signup form</h1>
+      <h2>Enter the captcha:</h2>
+      <label class="captcha">{captcha}</label>
+      <input type="text" value={value} onChange={inputHandler} />
+      <div>
+        <button type="submit">Submit</button>
+        <button onClick={regenerateCaptcha}>Refresh</button>
+      </div>
+    </form>
   );
-}
+};
 
 export default App;
